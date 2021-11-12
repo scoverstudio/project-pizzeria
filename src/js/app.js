31b9a252ceb325789aeef3b76a4265bcf63be8e7
Your Name <you@example.com>
2021-11-12 09:39:40 +0100

    add HTML to booking section

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,67 @@
 import {
   settings,
-  select
+  select,
+  classNames
 } from "./settings.js";
 
 import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
+import Booking from "./components/Booking.js";
+
+
 
 {
   "use strict";
 
   const app = {
+    initPages: function () {
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for (let page of thisApp.pages) {
+        if (page.id == idFromHash) {
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for (let link of thisApp.navLinks) {
+        link.addEventListener('click', function (event) {
+          const clickedElement = this;
+          event.preventDefault();
+
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          thisApp.activatePage(id);
+
+          window.location.hash = '#/' + id;
+        });
+      }
+    },
+
+    activatePage: function (pageId) {
+      const thisApp = this;
+
+      for (let page of thisApp.pages) {
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+
+      for (let link of thisApp.navLinks) {
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+
+    },
     initMenu: function () {
       const thisApp = this;
 
@@ -47,11 +99,18 @@ import Cart from "./components/Cart.js";
         app.cart.add(event.detail.product);
       });
     },
+    initBooking: function () {
+      const thisApp = this;
 
+      thisApp.bookingWidget = document.querySelector(select.containerOf.booking);
+      thisApp.booking = new Booking(thisApp.bookingWidget);
+    },
     init: function () {
       const thisApp = this;
+      thisApp.initPages();
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initBooking();
     },
   };
   app.init();
---
+++ src/js/components/Booking.js
@@ -0,0 +1,49 @@
+import {
+    templates,
+    select
+} from "../settings.js";
+import AmountWidget from "./AmountWidget.js";
+
+
+
+class Booking {
+    constructor(element) {
+        const thisBooking = this;
+
+        thisBooking.render(element);
+        thisBooking.initWidgets();
+    }
+
+    render(element) {
+        const thisBooking = this;
+
+        const generatedHTML = templates.bookingWidget();
+
+        thisBooking.dom = {};
+
+        thisBooking.dom.wrapper = element;
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+        thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+        thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    }
+
+    initWidgets() {
+        const thisBooking = this;
+
+        thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+        // thisBooking.dom.peopleAmount.addEventListener();
+        // thisBooking.dom.hoursAmount.addEventListener();
+    }
+}
+
+
+
+
+
+
+
+
+
+export default Booking;
