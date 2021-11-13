071d39fa415844b3bc540c75a844c7f0634cae7a
Your Name <you@example.com>
2021-11-13 10:25:42 +0100

    add hourPicker and datePicker to booking side

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -3,6 +3,8 @@ import {
     select
 } from "../settings.js";
 import AmountWidget from "./AmountWidget.js";
+import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
 
 
 
@@ -25,6 +27,8 @@ class Booking {
         thisBooking.dom.wrapper.innerHTML = generatedHTML;
         thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
     }
 
     initWidgets() {
@@ -32,6 +36,9 @@ class Booking {
 
         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
 
         // thisBooking.dom.peopleAmount.addEventListener();
         // thisBooking.dom.hoursAmount.addEventListener();
