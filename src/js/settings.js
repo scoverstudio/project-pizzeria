597eabb3f921a43559fb6e580ebba3ee7fefa9d6
Your Name <you@example.com>
2021-11-18 15:30:20 +0100

    small change

--- src/js/settings.js
+++ src/js/settings.js
@@ -118,7 +118,7 @@
           tableIdAttribute: 'data-table',
       },
       db: {
-          url: '//localhost:3131',
+          url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
           products: 'products',
           orders: 'orders',
           product: 'product',
